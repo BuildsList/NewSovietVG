22e5a3ceeea46d81b5e6dba04accf64ed5431d37
FighterX2500 <whitetes@yandex.ru>
2017-11-03 18:16:25 +0600

    Cyrillic

--- goon/browserassets/js/browserOutput.js
+++ goon/browserassets/js/browserOutput.js
@@ -509,12 +509,6 @@ if (typeof $ === 'undefined') {
 }
 
 $(function() {
-	// Detect encoding.
-	if (document.defaultCharset)
-	{
-		runByond("?_src_=chat&proc=encoding&encoding=" + escaper(document.defaultCharset));
-	}
-
 	$messages = $('#messages');
 	$subOptions = $('#subOptions');
 
